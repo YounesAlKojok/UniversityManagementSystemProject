ebe053f62325a5982d5b08e797b4cd8f3cf7a8ba
younes alkojok <younesalkojok@gmail.com>
2023-06-02 08:17:01 +0200

    1. remove method from appointment 2. show fist name of log in user mail 3. modify layout

--- UniversityManagementSystem/Models/Appoinment.cs
+++ UniversityManagementSystem/Models/Appoinment.cs
@@ -17,9 +17,9 @@ public partial class Appoinment
 
     public int StudentId { get; set; }
 
-    public virtual Dean Dean { get; set; } = null!;
+    public virtual Dean? Dean { get; set; } = null!;
 
-    public virtual Student Student { get; set; } = null!;
+    public virtual Student? Student { get; set; } = null!;
 
 
 
@@ -27,27 +27,30 @@ public partial class Appoinment
 
     public bool IsSlotActive { get; set; } = true;
 
-    public static List<DateTime> GetAvailableSlots()
-    {
-        var availableSlots = new List<DateTime>();
-        var startTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 12, 0, 0);
-
-        // Loop from 12:00 to 16:00 (Monday to Thursday)
-        for (int i = 0; i < 4; i++)
-        {
-            for (int j = 0; j < 3; j++)
-            {
-                var slotTime = startTime.AddMinutes(i * 20 + j * 5);
-
-                // Check if the slot is on Monday to Thursday
-                if (slotTime.DayOfWeek >= DayOfWeek.Monday && slotTime.DayOfWeek <= DayOfWeek.Thursday)
-                {
-                    availableSlots.Add(slotTime);
-                }
-            }
-        }
-
-        return availableSlots;
-    }
-
 }
+
+
+
+
+    //public static List<DateTime> GetAvailableSlots()
+    //{
+    //    var availableSlots = new List<DateTime>();
+    //    var startTime = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 12, 0, 0);
+
+    //    // Loop from 12:00 to 16:00 (Monday to Thursday)
+    //    for (int i = 0; i < 4; i++)
+    //    {
+    //        for (int j = 0; j < 3; j++)
+    //        {
+    //            var slotTime = startTime.AddMinutes(i * 20 + j * 5);
+
+    //            // Check if the slot is on Monday to Thursday
+    //            if (slotTime.DayOfWeek >= DayOfWeek.Monday && slotTime.DayOfWeek <= DayOfWeek.Thursday)
+    //            {
+    //                availableSlots.Add(slotTime);
+    //            }
+    //        }
+    //    }
+
+    //    return availableSlots;
+    //}
