4c03b62157a3940fa364afead866c20b72f0dc77
younes alkojok <younesalkojok@gmail.com>
2023-06-09 11:19:09 +0200

    1. modify help for footer 2. add help to home screen and navbar for new joiners

--- UniversityManagementSystem/Controllers/HomeController.cs
+++ UniversityManagementSystem/Controllers/HomeController.cs
@@ -26,6 +26,10 @@ namespace UniversityManagementSystem.Controllers
         {
             return View();
         }
+        public IActionResult Register()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
